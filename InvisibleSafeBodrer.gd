af93094922a0652be7b0efbce0c6d1e0db7b43d4
Amir Ehsandar <ehsandaramir@gamil.com>
2018-11-28 18:51:12 +0330

    clean

--- InvisibleSafeBodrer.gd
+++ InvisibleSafeBodrer.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Sprite
 
 # class member variables go here, for example:
 # var a = 2
@@ -8,4 +8,11 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
-	
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func hit(damage):
+	pass
